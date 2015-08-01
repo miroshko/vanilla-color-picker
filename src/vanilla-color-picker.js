8f95c04ac102713f708daf7b84106a6c54c02eda
Oleksii Miroshko <miroshko@live.com>
2015-08-02 01:51:22 +0200

    bump v0.1.6

--- src/vanilla-color-picker.js
+++ src/vanilla-color-picker.js
@@ -1,6 +1,6 @@
 // ============================================================
 //
-// Vanilla Color Picker v 0.1.4
+// Vanilla Color Picker v 0.1.6
 //
 // http://github.com/miroshko/vanilla-color-picker
 //
